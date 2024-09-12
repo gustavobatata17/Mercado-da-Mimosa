b619ce147d356d227eb437f4a7eb6550c6f0cadf
Gustavo José Costa Gregenski <133015470+gustavobatata17@users.noreply.github.com>
2024-09-12 09:15:11 -0300

    atualização básica

---
+++ Mercado-da-Mimosa-main/Mercadão/front_end/index.js
@@ -0,0 +1,10 @@
+let produto1 = document.getElementById('produto1')
+let produto2 = document.getElementById('produto2')
+let produto3 = document.getElementById('produto3')
+
+function teste() {
+    `<h1>${nome} </h1>
+    <img src="${rota_img}" alt="">
+    <p>${descricao}</p>
+    `
+}
